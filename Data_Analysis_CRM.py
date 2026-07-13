f811745aceec5faa399ad05e7d86b5ae0603ff63
Gonzalo Randazzo <npitiki@gmail.com>
2026-07-13 02:36:37 -0300

    Limpieza de bases relacionadas(Customers y Transactions) para tratamiento de la base principal (Reviews).

--- Data_Analysis_CRM.py
+++ Data_Analysis_CRM.py
@@ -37,3 +37,31 @@ else:
 # Al verificar duplicados y nulos, comenzamos con su corrección y depuración.
 #---------------------------------------------------
 
+#1. Cargamos la base de datos customers_limpio
+customers_limpio = pd.read_csv(r"A:\Escritorio\Proyectos DATA\CRM\end-to-end-saas-pipeline\customers_limpio.csv")
+
+# 2. Creamos el diccionario confiable (ahora sí, sin basura)
+diccionario_clientes = customers_limpio.set_index('customer_id')['full_name'].to_dict()
+
+# 3. Rescatamos los nulos mapeando el ID
+df_customer_reviews['full_name'] = df_customer_reviews['full_name'].fillna(
+    df_customer_reviews['customer_id'].map(diccionario_clientes)
+)
+
+print(f"Nulos restantes en full_name: {df_customer_reviews['full_name'].isnull().sum()}")
+
+#---------------------------------------------------
+# Los nulos restantes en full_name fueron depurados
+#---------------------------------------------------
+
+#----------------------------------------------------------------------------------------------------------
+# Traemos la df_transactions, ya que tiene los demás datos que necesitamos para purgar nuestra df objetivo
+#----------------------------------------------------------------------------------------------------------
+pd.read_csv(r'A:\Escritorio\Proyectos DATA\CRM\end-to-end-saas-pipeline\transactions_limpio.csv')
+print('¡Base de datos: transactions_limpio.csv leída con éxito!')
+
+#-------------------------------------------------------------------------------------------
+# Ahora, simplemente llamamos las columnas que queremos modificar con um merge (left join)
+#-------------------------------------------------------------------------------------------
+
+
